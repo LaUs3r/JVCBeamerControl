69bd2516b5cdd726e8122719b2c2d9e8357614ef
LaUs3r <laus3r@gmail.com>
2023-01-11 20:43:37 +0100

    Initial

---
+++ app/build/generated/source/buildConfig/debug/com/remotecontrol/jvcbeamercontrol/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.remotecontrol.jvcbeamercontrol;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.remotecontrol.jvcbeamercontrol";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
