0b5ff5b46912227fc84c2f194eaeb1e5f7be214b
LaUs3r <laus3r@gmail.com>
2023-01-22 17:56:45 +0100

    changed title and more colors

--- app/src/main/java/com/remotecontrol/jvcbeamercontrol/MainActivity.java
+++ app/src/main/java/com/remotecontrol/jvcbeamercontrol/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        setTitle("JVC BeamerControl");
+        setTitle("JVC ProjectorControl");
     }
 
     @Override
